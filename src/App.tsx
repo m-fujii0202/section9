b0e51b9af5fd297855d75591db6e64f610f9bce5
藤井真 <fuzimako0202@gmail.com>
2022-07-10 20:58:35 +0900

    セクション９

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Practice1 } from './practice/Practice1';
+import { Practice2 } from "./practice/Practics2";
+import { Practice3 } from './practice/Practics3';
+import { Practice4 } from './practice/Practics4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Practice1 />
+     <Practice2 />
+     <Practice3 />
+     <Practice4 />
     </div>
   );
 }
